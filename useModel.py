78be14b17f95841b46446133873dd6e525813e0f
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-02-07 16:08:36 -0800

    testing the new one

--- useModel.py
+++ useModel.py
@@ -5,10 +5,10 @@ with tf.Session() as sess:
     loadModel(sess, model_save_path)
 
     # batch = testSet.test_batch(batch_size)
-    batch = prepareImages(['results/testImage.png']*batch_size)
+    batch = prepareImages(['results/8.png']*batch_size)
     newBatch = sess.run([view, vox], feed_dict={
         # view: batch[0]
         view: batch
     })
 
-    saveResults(newBatch, saveImages=False)
+    saveResults(newBatch, fileName='8.pkl', saveImages=False)
