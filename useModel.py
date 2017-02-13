96ec7a948b3302653725ece4cc0d80d04072f3f3
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-02-13 14:08:01 -0800

    testing after 30k cycles of training

--- useModel.py
+++ useModel.py
@@ -5,15 +5,15 @@ ballDataset = dataset('ball_dataset/')
 with tf.Session() as sess:
     loadModel(sess, model_save_path[1])
 
-    batch = ballDataset.test_batch(batch_size*12)
+    batch = ballDataset.test_batch(batch_size*8)
     batch = ballDataset.test_batch(batch_size)
     # imgs = prepareImages(batch[0])
     newBatch = sess.run([view, vox], feed_dict={
         view: batch[0]
     })
 
-    saveResults(newBatch, fileName='vox_2.pkl', saveImages=False)
+    saveResults(newBatch, fileName='vox.pkl', saveImages=False)
 
     for i in range(batch_size):
         img = toImage(batch[0][i:i+1])
-        img.save('results_ball/%s.png'%(i+5))
+        img.save('results_ball/%s.png'%(i))
