5da4254814f33560412223a7a01dc6abc66908af
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-02-12 20:48:22 -0800

    changed this file to use the model and test it

--- useModel.py
+++ useModel.py
@@ -1,14 +1,19 @@
 from model import *
 
 # testSet = dataset('results/')
+ballDataset = dataset('ball_dataset/')
 with tf.Session() as sess:
     loadModel(sess, model_save_path[1])
 
-    # batch = testSet.test_batch(batch_size)
-    batch = prepareImages(['ball_dataset/8.png']*batch_size)
+    batch = ballDataset.test_batch(batch_size*12)
+    batch = ballDataset.test_batch(batch_size)
+    # imgs = prepareImages(batch[0])
     newBatch = sess.run([view, vox], feed_dict={
-        # view: batch[0]
-        view: batch
+        view: batch[0]
     })
 
-    saveResults(newBatch, fileName='vox.pkl', saveImages=False)
+    saveResults(newBatch, fileName='vox_2.pkl', saveImages=False)
+
+    for i in range(batch_size):
+        img = toImage(batch[0][i:i+1])
+        img.save('results_ball/%s.png'%(i+5))
