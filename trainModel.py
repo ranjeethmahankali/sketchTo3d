83356b1f27d76ab8df3f21cf0a80db8a78534e0c
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-04-19 18:55:58 -0700

    training further

--- trainModel.py
+++ trainModel.py
@@ -7,10 +7,10 @@ ballDataset = dataset('ball_dataset/')
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     # deleting old logs and setting up new ones
-    shutil.rmtree(log_dir, ignore_errors=True)
+    # shutil.rmtree(log_dir, ignore_errors=True)
     train_writer, test_writer = getSummaryWriters(sess)
     # loadModel(sess, model_save_path[0])
-    # loadModel(sess, model_save_path[1])
+    loadModel(sess, model_save_path[1])
 
     cycles = 2000
     testStep = 40
@@ -24,7 +24,7 @@ with tf.Session() as sess:
                 view: batch[0],
                 voxTrue: batch[1]
             })
-            if i % log_step == 0: train_writer.add_summary(summary, i)
+            if i % log_step == 0: train_writer.add_summary(summary,2000+i)
 
             timer = estimate_time(startTime, cycles, i)
             pL = 10 # this is the length of the progress bar to be displayed
@@ -39,7 +39,7 @@ with tf.Session() as sess:
                     view: testBatch[0],
                     voxTrue: testBatch[1]
                 })
-                test_writer.add_summary(summary, i)
+                test_writer.add_summary(summary, 2000+i)
                 print('Accuracy: %.2f; Sums: %.2f, %.2f%s'%(acc,v.sum(),testBatch[1].sum(),' '*50))
         
         # now saving the trained model every 1500 cycles
