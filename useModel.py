5afa691a50a0a6441abb44bc30f1cd6b64280cdf
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2018-01-16 19:10:10 -0800

    testing the last trained version of the model

--- useModel.py
+++ useModel.py
@@ -10,12 +10,13 @@ with tf.Session() as sess:
     batch = testSet.test_batch(batch_size*14)
     batch = testSet.test_batch(batch_size)
     # imgs = prepareImages(batch[0])
-    newBatch = sess.run([view, vox], feed_dict={
+    newBatch = sess.run([view, vox, accuracy], feed_dict={
         view: batch[0]
     })
 
-    saveResults(newBatch, fileName='vox.pkl', saveImages=False)
-
+    saveResults(newBatch[:2], fileName='vox.pkl', saveImages=False)
+    acc = newBatch[2]
+    print("Accuracy: %.2f"%np.mean(acc))
     for i in range(batch_size):
         # img = toImage(batch[0][i:i+1], imSize = [96,128])
         img = toImage(batch[0][i:i+1])
